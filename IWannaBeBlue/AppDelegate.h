b5813419d4db777ab5940000dbc4bb121fccbc62
bmathien <bmathien@barrington220.org>
2014-05-12 13:33:10 -0500

    Initial Commit

---
+++ IWannaBeBlue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IWannaBeBlue
+//
+//  Created by teacher on 5/12/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IWannaBeBlue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IWannaBeBlue
+//
+//  Created by teacher on 5/12/14.
+//  Copyright (c) 2014 Mathien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
